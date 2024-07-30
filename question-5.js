b40f69a080304ee126e36287b85a7b3b45158e9c
KeeratiNon <keeratinon3@gmail.com>
2024-07-30 09:51:11 +0700

    answer ex1-5

--- question-5.js
+++ question-5.js
@@ -2,5 +2,6 @@
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
 
+Big O = O(n^2) เพราะมีการใช้ loop ซ้อน loop ทำให้จำนวนรอบในการเช็คเพิ่มขึ้นในรูปแบบยกกำลัง 2
 
 */
